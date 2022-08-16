49b3d87623c933e8c2599da72af2d341fbd8f3e5
ttrendsetter <klo4kov.99@gmail.com>
2022-08-16 16:30:51 +0300

    posts done

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,35 @@
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Post
+from django.urls import reverse_lazy
 
 
-class BlogDetailVew(DetailView):
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'post_new.html'
+    fields = '__all__'
+
+
+class BlogDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
     context_object_name = 'Post'
 
-
 class BlogListView(ListView):
     model = Post
     template_name = 'home.html'
+
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_edit.html'
+    fields = ['title', 'body']
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    context_object_name = 'Post'
+    success_url = reverse_lazy('home')
+
+
 # Create your views here.
